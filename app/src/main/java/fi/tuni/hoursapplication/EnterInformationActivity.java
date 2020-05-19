6152bc6249ec099fa31da97d78b278c7d8ecacca
annametsapelto <anna.metsapelto@gmail.com>
2020-05-19 21:29:57 +0300

    Add two way data exchange

--- app/src/main/java/fi/tuni/hoursapplication/EnterInformationActivity.java
+++ app/src/main/java/fi/tuni/hoursapplication/EnterInformationActivity.java
@@ -5,16 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EnterInformationActivity extends AppCompatActivity {
+    EditText hText;
+    EditText mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_information);
-    }
-    public void enterInformation(View v) {
-        Intent intent = new Intent(this, MainViewActivity.class);
-        startActivity(intent);
+        mText = findViewById(R.id.minutesInput);
+        hText = findViewById(R.id.hoursInput);
+        Button button = findViewById(R.id.returnButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hText.getText().toString().equals("") || mText.getText().toString().equals("")) {
+                    Toast.makeText(EnterInformationActivity.this, "Enter hours and minutes.", Toast.LENGTH_SHORT).show();
+                } else {
+                    int h = Integer.parseInt(hText.toString());
+                    int m = Integer.parseInt(mText.toString());
+                    Intent intent = new Intent()
+                            .putExtra("hours", h)
+                            .putExtra("minutes", m);
+
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/fi/tuni/hoursapplication/MainViewActivity.java
+++ app/src/main/java/fi/tuni/hoursapplication/MainViewActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainViewActivity extends AppCompatActivity {
     private int hours;
     private int minutes;
     private String totalTime;
+    TextView totalTimeText;
+    private static final int REQUEST_CODE = 1;
 
     public String getTotalTime() {
         return totalTime;
@@ -47,11 +50,31 @@ public class MainViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTotalTime(getHours(), getMinutes() );
         setContentView(R.layout.activity_main_view);
+        totalTimeText = findViewById(R.id.totalWorkTime);
+        totalTimeText.setText("Total working hours: " + getTotalTime());
     }
 
     public void enteringView(View v) {
         Intent intent = new Intent(this, EnterInformationActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Bundle bundle = data.getExtras();
+                setHours(bundle.getInt("hours"));
+                setMinutes(bundle.getInt("minutes"));
+                setTotalTime(getHours(), getMinutes());
+                totalTimeText.setText("Total work time: " + getTotalTime());
+            }
+            if (resultCode == RESULT_CANCELED) {
+
+            }
+        }
+    }
+
 }
