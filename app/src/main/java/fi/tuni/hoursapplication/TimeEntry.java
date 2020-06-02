2c179641bf0a9f5519b5b0a684b94fe1f7aa615d
annametsapelto <anna.metsapelto@gmail.com>
2020-06-02 18:25:18 +0300

    Create TimeEntry class.

---
+++ app/src/main/java/fi/tuni/hoursapplication/TimeEntry.java
@@ -0,0 +1,34 @@
+package fi.tuni.hoursapplication;
+
+/* This class contains TimeEntry which consists of String date, int hours and int minutes
+
+ */
+public class TimeEntry {
+    String date;
+    int hours;
+    int minutes;
+
+    public TimeEntry(String date, int hours, int minutes) {
+        setDate(date);
+        setHours(hours);
+        setMinutes(minutes);
+    }
+    public void setDate(String d) {
+        date=d;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setHours(int h) {
+        hours = h;
+    }
+    public int getHours() {
+        return hours;
+    }
+    public void setMinutes(int m) {
+        minutes = m;
+    }
+    public int getMinutes() {
+        return minutes;
+    }
+}
